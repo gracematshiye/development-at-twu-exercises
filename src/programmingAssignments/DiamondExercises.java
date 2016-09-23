13e111733f3bd4262e5462b4ef4fcb0db01dac66
Grace <gracem@thoughtworks.com>
2016-09-23 10:03:11 +0200

    Completed the Diamond with Name exercise

--- src/programmingAssignments/DiamondExercises.java
+++ src/programmingAssignments/DiamondExercises.java
@@ -50,7 +50,39 @@ public class DiamondExercises {
             System.out.println("");
         }
 
+        System.out.println("\n");
 
+        //Diamond with Name
+
+        for(int i=1; i<n; i++) {
+
+            for(int j=2; j>i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            for(int k=1; k<=(2*i-1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+        System.out.println("Bill");
+        for(int i=n-1; i>=1; i--) {
+
+            for(int j=2; j>i; j--)
+            {
+                System.out.print(" ");
+            }
+
+            for(int k=1; k<(2*i); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+
+        System.out.println("\n");
 
     }
 }
